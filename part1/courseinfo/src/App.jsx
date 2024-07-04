7d9b6653cdd06ea91e691677715af227a3e777bc
Ken <kenxhen@gmail.com>
2024-07-04 17:45:17 +0800

    1.4 Add array of objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,13 +18,17 @@ const Part = (part) => {
   )
 }
 
+/* What is the problem now?
+i want to divide into 3 parts the array
+
+*/
 const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1}></Part>
-      <Part name={props.part2} exercises={props.exercises2}></Part>
-      <Part name={props.part3} exercises={props.exercises3}></Part>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}></Part>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}></Part>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}></Part>
     </div>
 
   )
@@ -34,36 +38,34 @@ const Content = (props) => {
 const Footer = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course = {course}></Header>
-      <Content 
-        part1 = {part1.name} exercises1 = {part1.exercises}
-        part2 = {part2.name} exercises2 = {part2.exercises}
-        part3 = {part3.name} exercises3 = {part3.exercises}
-        ></Content>
-      <Footer exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}>
-      </Footer>
+      <Content parts={parts} />
+      <Footer parts = {parts} />
+      
 
     </div>
 
