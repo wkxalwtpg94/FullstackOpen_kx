3c1405553998c6371fa33223faeb6fa05fc4bb16
Ken <kenxhen@gmail.com>
2024-07-12 13:38:26 +0800

    Implement filter functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "040-1234567"
-    }
+    { name: 'Arto Hellas', number: "040-1234567"},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [newName, setNewName] = useState('a new name...')
   const [newNumber, setNewNumber] = useState("012-XX40523")
+  const [newFilter, setNewFilter] = useState("")
 
   console.log("checking the current list of persons:", persons)
 
@@ -52,9 +53,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+ // handle Filtern onChange
+  
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  let personsToShow;
+  if (newFilter === "") {
+    personsToShow = persons
+  } else {
+    personsToShow = persons.filter((person)=> {return (person.name.toLowerCase().startsWith(newFilter.toLowerCase()))})
+  }
+    console.log(personsToShow)
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>Filter names with: <input value ={newFilter} onChange={handleFilterChange}></input></div>
+      <h2>Add New Contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value = {newName} onChange={handleNameChange}/>
@@ -68,9 +87,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
         <div>
-          {persons.map((person, index) => <p key={`${person.name}-${index}`}> {person.name} {person.number}</p>)}
-          
-
+          {personsToShow.map((person) => <p key={person.name}>{person.name} {person.number} </p>)}
         </div>
     </div>
   )
